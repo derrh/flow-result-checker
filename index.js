c925504dd6311c4d2142190497289a99b2be46e6
Derrick Hathaway <derr@me.com>
2017-09-18 11:46:34 -0600

    Captures all of Flow's output before processing

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 const checker = require('./checker')()
 
 process.stdin.setEncoding('utf8')
-process.stdin.on('data', (stdout) => checker({ stdout }))
+let flowOutput = ''
+process.stdin.on('data', (stdout) => flowOutput += stdout)
+process.on('exit', () => checker({ stdout: flowOutput }))
+
